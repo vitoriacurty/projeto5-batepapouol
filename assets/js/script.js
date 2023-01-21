6f7d4c9e2a52b9138ba0b530b0cc48e2d575d11b
vitoriacurty <vitoria.curty@hotmail.com>
2023-01-21 18:38:00 -0300

    correções JavaScript

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,9 +5,6 @@ let batePapo = document.querySelector('.chat')
 const mensagemDigitada = document.querySelector('.text')
 verificarUsuario()
 
-// let batePapo = document.querySelector('.chat')
-// const msg = document.querySelector(".text input")
-
 function verificarUsuario() {
   const resposta = axios.post(
     'https://mock-api.driven.com.br/api/v6/uol/participants',
@@ -21,16 +18,16 @@ function mantendoConexao(){
   manterConexao.then()
   manterConexao.catch()
 }
-// setInterval(mantendoConexao, 5000)
 
 function respostaChegou(resposta) {
   let batePapo = document.querySelector('.chat')
   batePapo.innerHTML = ''
+
   for (let i = 0; i < 100; i++) {
     let hora = resposta.data[i].time
     let nome1 = resposta.data[i].from
     let nome2 = resposta.data[i].to
-    let texto = resposta.data[i].texto
+    let texto = resposta.data[i].text
     let tipo = resposta.data[i].type
 
     if (tipo === 'status' || tipo === 'message' ){
@@ -40,7 +37,7 @@ function respostaChegou(resposta) {
       
       <div data-test="message" class="${tipo}">
           (${hora}) ${nome1} para ${nome2}: ${texto}
-      </div> <!-- fechamento entra na sala -->
+      </div> 
   
       `;
   }
@@ -52,12 +49,16 @@ function respostaChegou(resposta) {
       
       <div data-test="message" class="${tipo}">
           (${hora}) ${nome1} para ${nome2}: ${texto}
-      </div> <!-- fechamento entra na sala -->
+      </div> 
   
       `;
   }
 }
   batePapo.querySelector('div:last-child').scrollIntoView()
+}
+
+function deuErro(erro){
+  window.location.reload()
 }
 
 function pegarConversaNoServidor() {
@@ -66,7 +67,7 @@ function pegarConversaNoServidor() {
   )
 
   promessa.then(respostaChegou)
-  promessa.catch(erro)
+  promessa.catch(deuErro)
 }
 
 function enviarMensagem(){
@@ -83,7 +84,7 @@ function enviarMensagem(){
   mensagemDigitada.value=""
 
   enviar.then(pegarConversaNoServidor);
-  enviar.catch(erro);
+  enviar.catch(deuErro);
 }
 
 function usuarioVerificado (){
@@ -95,32 +96,4 @@ function usuarioVerificado (){
   setInterval(mantendoConexao, 5000);
 }
 
-  function pegarConversaNoServidor(){
-
-    const promessa = axios.get('https://mock-api.driven.com.br/api/v6/uol/messages');
-
-    promessa.then(respostaChegou);
-    promessa.catch(erro);
-}
-
-function enviarMensagem(){
-
-
-    const msg = {
-        from: nome,
-        to: "Todos",
-        text: mensagemDigitada.value,
-        type: "message" 
-    }
-
-    const enviar = axios.post('https://mock-api.driven.com.br/api/v6/uol/messages', msg);
-
-    mensagemDigitada.value = "";
-
-    enviar.then(pegarConversaNoServidor);
-    enviar.catch(erro);
-}
-
-function erro (erro){
-  window.location.reload();
-}
+  
